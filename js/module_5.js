a7f2c7844a08a72ed8edb5cd4cbd3257e624a235
Sergey Misyuk <smisyuk@gmail.com>
2022-09-20 01:14:40 +0300

    tasks 11-20

--- js/module_5.js
+++ js/module_5.js
@@ -170,7 +170,7 @@
 //     balance: 24000,
 //     discount: 0.1,
 //     orders: ["Burger", "Pizza", "Salad"],
-    
+
 //     getBalance() {
 //         return this.balance;
 //     },
@@ -304,7 +304,7 @@
 
 //     addItem(newItem){
 //         this.items.push(newItem); //1 метод
-        // this.items = [...this.items, newItem]; //2 метод
+// this.items = [...this.items, newItem]; //2 метод
 //     }
 
 //     removeItem(itemToRemove){
@@ -324,7 +324,347 @@
 
 
 // ========= ======= === WOME WORK - 11
+// class StringBuilder{
+//         constructor(initialValue){
+//                 this.value = initialValue;
+//         }
+
+//         padStart(str) {
+//                 this.value = str + this.value;
+//         }
+
+//         padEnd(str) {
+//                 this.value = this.value + str;
+//         }
+
+//         padBoth(str) {
+//                 this.value = str + this.value + str;
+
+                //alternative code
+                //this.padStart(str);
+                //this.padEnd(str);
+//         }
+
+//         getValue() {
+//                 return this.value;
+//         }
+// }
 
 
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
 
+
+// ========= ======= === WOME WORK - 12
+// class Car {
+//         #brand;
+
+//         constructor({ brand, model, price }) {
+//                 this.#brand = brand;
+//                 this.model = model;
+//                 this.price = price;
+//         }
+
+//         getBrand(){
+//                 return this.#brand;
+//         }
+
+//         changeBrand(newBrand){
+//                 this.#brand = newBrand;
+//         }
+// }
+
+// const audi = new Car({ brand: "Audi", model: "Q3", price: 36000 }); // { model: "Q3", price: 36000 }
+// console.log(audi);
+
+// const bmw = new Car({ brand: "bmw", model: "X5", price: 58900 }); // { model: "X5", price: 58900 }
+// console.log(bmw);
+
+// const nissan = new Car({ brand: "Nissan", model: "Murano", price: 31700 }); // { model: "Murano", price: 31700 }
+// console.log(nissan);
+
+// nissan.changeBrand("Honda"); //не работает
+// console.log(nissan);
+
+
+// ========= ======= === WOME WORK - 13
+// class Storage {
+//         #items;
+
+//         constructor(items) {
+//                 this.#items = items;
+//         }
+
+//         getItems() {
+//                 return this.#items;
+//         }
+
+//         addItem(newItem) {
+//                 this.#items.push(newItem);
+//         }
+
+//         removeItem(itemToRemove) {
+//                 this.#items = this.#items.filter(item => item !== itemToRemove);
+//         }
+// }
+
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+
+
+// ========= ======= === WOME WORK - 14
+// class StringBuilder {
+//         #value;
+
+//         constructor(initialValue) {
+//                 this.#value = initialValue;
+//         }
+
+//         getValue() {
+//                 return this.#value;
+//         }
+
+//         padEnd(str) {
+//                 this.#value += str;
+//         }
+
+//         padStart(str) {
+//                 this.#value = str + this.#value;
+//         }
+
+//         padBoth(str) {
+//                 this.padStart(str);
+//                 this.padEnd(str);
+//         }
+// }
+
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+
+// ========= ======= === WOME WORK - 15
+// class Car {
+//         #brand;
+//         #model;
+//         #price;
+
+//         constructor({ brand, model, price }) {
+//                 this.#brand = brand;
+//                 this.#model = model;
+//                 this.#price = price;
+//         }
+
+//         get brand() {
+//                 return this.#brand;
+//         }
+
+//         set brand(newBrand) {
+//                 this.#brand = newBrand;
+//         }
+
+//         get model() {
+//                 return this.#model;
+//         }
+
+//         set model(newModel) {
+//                 this.#model = newModel;
+//         }
+
+//         get price() {
+//                 return this.#price;
+//         }
+
+//         set price(newPrice) {
+//                 this.#price = newPrice;
+//         }
+// }
+
+// const nissan = new Car({ brand: "Nissan", model: "Murano", price: 31700 }); // { model: "Murano", price: 31700 }
+// console.log(nissan); // не показывает свойства объекта потому что они личные
+
+
+// ========= ======= === WOME WORK - 16
+// class Car {
+//         static MAX_PRICE = 50000;
+
+//         #price;
+
+//         constructor({ price }) {
+//                 this.#price = price;
+//         }
+
+//         get price() {
+//                 return this.#price;
+//         }
+
+//         set price(newPrice) {
+//                 if (Car.MAX_PRICE >= newPrice)
+//                         this.#price = newPrice;
+//         }
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+
+// ========= ======= === WOME WORK - 17
+// class Car {
+//         static #MAX_PRICE = 50000;
+
+//         static checkPrice(price) {
+//                 if (price > Car.#MAX_PRICE) {
+//                         return "Error! Price exceeds the maximum";
+//                 }
+
+//                 return "Success! Price is within acceptable limits";
+//         }
+
+//         constructor({ price }) {
+//                 this.price = price;
+//         }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+
+// ========= ======= === WOME WORK - 18
+// class User {
+//         constructor(email) {
+//                 this.email = email;
+//         }
+
+//         get email() {
+//                 return this.email;
+//         }
+
+//         set email(newEmail) {
+//                 this.email = newEmail;
+//         }
+// }
+
+// class Admin extends User {
+//         static AccessLevel = {
+//                 BASIC: "basic",
+//                 SUPERUSER: "superuser",
+//         }
+// }
+
+
+// ========= ======= === WOME WORK - 19
+// class User {
+//         email;
+
+//         constructor(email) {
+//                 this.email = email;
+//         }
+
+//         get email() {
+//                 return this.email;
+//         }
+
+//         set email(newEmail) {
+//                 this.email = newEmail;
+//         }
+// }
+
+// class Admin extends User {
+//         constructor({ email, accessLevel }) {
+//                 super(email);
+
+//                 this.email = email;
+//                 this.accessLevel = accessLevel;
+//         }
+
+//         static AccessLevel = {
+//                 BASIC: "basic",
+//                 SUPERUSER: "superuser",
+//         };
+// }
+
+// const mango = new Admin({
+//         email: "mango@mail.com",
+//         accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+
+// ========= ======= === WOME WORK - 20
+// class User {
+//         email;
+
+//         constructor(email) {
+//                 this.email = email;
+//         }
+
+//         get email() {
+//                 return this.email;
+//         }
+
+//         set email(newEmail) {
+//                 this.email = newEmail;
+//         }
+// }
+// class Admin extends User {
+//         blacklistedEmails = [];        
+
+//         static AccessLevel = {
+//                 BASIC: "basic",
+//                 SUPERUSER: "superuser",
+//         };
+
+//         constructor({ email, accessLevel }) {
+//                 super(email);
+
+//                 this.accessLevel = accessLevel;
+//         }
+
+//         blacklist(email) {
+//                 this.blacklistedEmails = [...this.blacklistedEmails, email];
+//         };
+
+//         isBlacklisted(email) {
+//                 return this.blacklistedEmails.includes(email);
+//         }
+// }
+
+// const mango = new Admin({
+//         email: "mango@mail.com",
+//         accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
 
