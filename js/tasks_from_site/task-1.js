d4fa3303a2ace980c44db4ed19fe7ee82cd527a8
Sergey Misyuk <smisyuk@gmail.com>
2022-09-26 17:02:51 +0300

    task-1

--- js/tasks_from_site/task-1.js
+++ js/tasks_from_site/task-1.js
@@ -1,2 +1,16 @@
 "use strict";
 
+// Напишіть такий JavaScript,
+// щоб після натискання на кнопку button,
+// елемент < div id = "text" > зникав.
+
+const refs = {
+    btn: document.querySelector('#hider'),
+    elem: document.querySelector('#text'),
+}
+
+const onClickRemoveBtn = () => {
+    refs.elem.remove();
+}
+
+refs.btn.addEventListener('click', onClickRemoveBtn)
