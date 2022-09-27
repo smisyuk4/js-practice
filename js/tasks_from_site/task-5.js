67877fd6d35f98bf4ca9070593cc07e12c52862f
Sergey Misyuk <smisyuk@gmail.com>
2022-09-28 00:52:18 +0300

    start task-5 carousel

--- js/tasks_from_site/task-5.js
+++ js/tasks_from_site/task-5.js
@@ -1,3 +1,15 @@
 "use strict";
 
-//
+//Створити “Карусель” – стрічку зображень,
+//яку можна прокручувати,
+//натискаючи на стрілки.
+
+// цей код позначає картинки, для зручності розробки
+// він може бути видалений
+let i = 1;
+for(let li of document.querySelectorAll('li')) {
+    li.style.position = 'relative';
+    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+    i++;
+}
+// ...ваш код, який зробить карусель робочою
