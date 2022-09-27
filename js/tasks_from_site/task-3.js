383c8f8a09e9544c1908a0207f28a17e2d2e2238
Sergey Misyuk <smisyuk@gmail.com>
2022-09-27 20:27:35 +0300

    hide menu onClick

--- js/tasks_from_site/task-3.js
+++ js/tasks_from_site/task-3.js
@@ -2,3 +2,14 @@
 
 //Створіть меню, яке відкривається/згортається після кліку:
 
+const refs = {
+    menu: document.querySelector('.menu'),
+    title: document.querySelector('.title'),
+    menuItems: document.querySelector('.menu ul'),
+}
+
+const onClickTitle = () => {
+    refs.menuItems.classList.toggle('hidden')
+}
+
+refs.title.addEventListener('click', onClickTitle)
