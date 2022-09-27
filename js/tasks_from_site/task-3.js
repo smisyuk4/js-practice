96d0c97be93c29ce7b286dcb7b0eef38d4ca6890
Sergey Misyuk <smisyuk@gmail.com>
2022-09-27 20:55:19 +0300

    task-3 - show items menu done

--- js/tasks_from_site/task-3.js
+++ js/tasks_from_site/task-3.js
@@ -5,11 +5,11 @@
 const refs = {
     menu: document.querySelector('.menu'),
     title: document.querySelector('.title'),
-    menuItems: document.querySelector('.menu ul'),
+    // menuItems: document.querySelector('.menu ul'),
 }
 
 const onClickTitle = () => {
-    refs.menuItems.classList.toggle('hidden')
+    refs.menu.classList.toggle('hidden')
 }
 
 refs.title.addEventListener('click', onClickTitle)
