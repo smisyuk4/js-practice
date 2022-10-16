62d2afb32611b9840c27ae4105f1cbf49babb7e1
Sergey Misyuk <smisyuk@gmail.com>
2022-10-16 23:59:45 +0300

    done task

--- js/task-5.js
+++ js/task-5.js
@@ -13,3 +13,58 @@ for(let li of document.querySelectorAll('li')) {
     i++;
 }
 // ...ваш код, який зробить карусель робочою
+
+const refs = {
+    arrowLeft: document.querySelectorAll('.arrow')[0],
+    arrowRight: document.querySelectorAll('.arrow')[1],
+    images: document.querySelector('ul'),
+    items: document.querySelectorAll('ul li'),
+}
+// let currentNum = 0;
+
+// const onClickButtonLeft = (event) => {
+//     console.log(event.target)
+// }
+
+
+
+// const onClickButtonRight = (event) => {
+//     console.log(event.target)
+//     console.log(refs.items)
+    
+//     for (let item of refs.items) {
+//         item.classList.add('hide')
+//     }
+
+//     refs.items[currentNum].classList.remove('hide')
+//     refs.items[currentNum+1].classList.remove('hide')
+//     refs.items[currentNum+2].classList.remove('hide')
+    
+//     if (currentNum + 3 === refs.items.length) {
+//         currentNum = 0;
+//     } else {
+//         currentNum++;
+//     }
+    
+// }
+
+//решение с сайта
+const widthPic = 130;
+const countPic = 1;
+let position = 0;
+
+const onClickButtonLeft = (event) => {
+    position += widthPic * countPic;
+    position = Math.min(position, 0);
+    refs.images.style.marginLeft = `${position}px`;
+}
+
+const onClickButtonRight = (event) => {
+    position -= widthPic * countPic;
+    position = Math.max(position, -widthPic * (refs.items.length - countPic))
+    refs.images.style.marginLeft = `${position}px`;
+}
+
+refs.arrowLeft.addEventListener('click', onClickButtonLeft)
+
+refs.arrowRight.addEventListener('click', onClickButtonRight)
