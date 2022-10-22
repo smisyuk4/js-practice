fa81853824625d89ac6fd3e85ba4579f4e1a8fec
Sergey Misyuk <smisyuk@gmail.com>
2022-10-22 22:17:37 +0300

    second gallery done

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,18 @@
-const basicLightbox = require('basiclightbox')
+// import * as basicLightbox from 'basiclightbox'
 
-import * as basicLightbox from 'basiclightbox'
+
 
 const refs = {
     gallery: document.querySelector('.gallery'),
+}
+
+let instance = {};
+
+const onClickEsc = (event) => {
+    if (event.code === 'Escape') {
+        instance.close();
+        window.removeEventListener('keyup', onClickEsc)
+    }
 }
 
 const onClickImage = (event) => {
@@ -13,10 +22,15 @@ const onClickImage = (event) => {
         return;
     }
 
-    const instance = basicLightbox.create(`
-        <img src="assets/images/image.png" width="800" height="600">
+    const largeImage = event.target.getAttribute('data-link');
+
+    instance = basicLightbox.create(`
+        <img id="large-img" src="${largeImage}">
     `)
+    
     instance.show()
+
+    window.addEventListener('keyup', onClickEsc)
 }
 
 refs.gallery.addEventListener('click', onClickImage);
