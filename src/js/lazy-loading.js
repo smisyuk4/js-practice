a4b46b3f4991448a6225be743d5b578859507e2d
Sergey Misyuk <smisyuk@gmail.com>
2022-11-01 00:45:31 +0200

    lazysizes

--- src/js/lazy-loading.js
+++ src/js/lazy-loading.js
@@ -2,12 +2,21 @@
 // https://github.com/aFarkas/lazysizes/blob/gh-pages/README.md
 // npm install lazysizes --save
 
+// once: Boolean указывает, что обработчик должен быть вызван не более одного раза 
+// после добавления. Если true, обработчик автоматически удаляется при вызове.
+
 import 'lazysizes';
 // import a plugin
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
-// Note: Never import/require the *.min.js files from the npm package.
+const refs = {
+    images: document.querySelectorAll('.feed-img'),
+}
 
+refs.images.forEach(image => {
+    image.addEventListener('load', onLoadImage, {once: true})
+})
 
-
-
+function onLoadImage(event) {
+    event.target.classList.add('appear')
+}
