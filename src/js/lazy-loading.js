eaefe56ec0712f5255ce78d1c82a4405607afc48
Sergey Misyuk <smisyuk@gmail.com>
2022-10-31 23:53:50 +0200

    lazysizes

---
+++ src/js/lazy-loading.js
@@ -0,0 +1,13 @@
+// https://afarkas.github.io/lazysizes/index.html
+// https://github.com/aFarkas/lazysizes/blob/gh-pages/README.md
+// npm install lazysizes --save
+
+import 'lazysizes';
+// import a plugin
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
+// Note: Never import/require the *.min.js files from the npm package.
+
+
+
+
