cdaae28153dec3bc06fbeb29d67235abcfa8c340
Sergey Misyuk <smisyuk@gmail.com>
2022-11-10 23:56:04 +0200

    make form for user

---
+++ src/js/crud-operations.js
@@ -0,0 +1,10 @@
+//        сервер для практики запросов
+// https://github.com/typicode/json-server
+// npm install -g json-server
+
+//        запуск сервера
+// npm run server (потому что сделал скрипт запуска сервера)
+//        или базовая команда от пакета
+// json-server --watch db.json --port 3004
+
+//  http://localhost:3000/posts/1,
