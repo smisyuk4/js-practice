ff4fcc48bb750830507b5cad4e1c5b7f228c338a
Sergey Misyuk <smisyuk@gmail.com>
2022-11-11 01:21:19 +0200

    make collection from form by user

--- src/js/crud-operations.js
+++ src/js/crud-operations.js
@@ -8,3 +8,54 @@
 // json-server --watch db.json --port 3004
 
 //  http://localhost:3000/posts/1,
+
+// class formData
+//https://www.youtube.com/watch?v=iJq-NwbZL84
+
+const refs = {
+    userForm: document.querySelector('.user-form'),
+    sendBtn: document.querySelector('.user-form button'),
+    sortBtns: document.querySelector('.sort-buttons'),
+    userBase: document.querySelector('.user-base'),
+}
+
+refs.userForm.addEventListener('input', onInputChanges)
+
+refs.userForm.addEventListener('submit', onClickSendBtn)
+
+const userData = {}
+
+function onClickSendBtn(event) {
+    event.preventDefault();
+
+    let skills = []    
+    const formData = new FormData(event.currentTarget)
+
+    formData.forEach((value, name) => {
+        if (name !== 'skills') {
+            userData[name] = value
+        } else {
+            skills = [...skills, value]
+            userData[name] = skills
+        }        
+    })
+
+    //send userData to storage
+    console.log(userData)
+    event.target.reset()
+}
+
+function onInputChanges(event) {
+    let length = null
+    const formData = new FormData(event.currentTarget)
+
+    formData.forEach((value, name) => {
+        if (value !== '') {
+            length += 1
+        }
+    })
+
+    if (length === 4) {
+        refs.sendBtn.removeAttribute('disabled')
+    }
+}
